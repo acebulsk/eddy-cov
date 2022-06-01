09432d4d46bc0aa15e8204ded2c8093082ce1f26
Alex Cebulski <alexcebulski@gmail.com>
2022-06-01 16:25:01 -0600

    init

--- scripts/append_15min.R
+++ scripts/append_15min.R
@@ -1,15 +1,30 @@
-# combine all 15 min EC downloads
+# combine all 15 min EC downloads, this script uses the output from the campbell sci card 
+# convert program which extracts the compressed TOA3 files to TOA5 which we load in here
+# hi freq data was moved to hard drive (compressed data on hard drive and one drive)
+library(dplyr)
+library(wxlogR)
 
-ec1 <- wxlogR::load_CS_1000(path = '../../field-downloads/fortress/hanging tree/hi freq/clean/TOA5_FRG_20211214.flux15.dat',
-                            skip_4_dat = 4)
+# get just the 15 min data
+files <- list.files('../../field-downloads/met-data/hi-freq/clean', pattern = '*flux15.dat', full.names = T)
 
-ec2 <- wxlogR::load_CS_1000(path = '../../field-downloads/fortress/hanging tree/hi freq/clean/TOA5_FRG_20220127.flux15.dat',
-                            skip_4_dat = 4)
+# loop through files and bind together
+bind <- map_dfr(files, wxlogR::load_CS_1000, skip_4_dat = 4, timezone = 'CST') |> distinct()
 
-ec3 <- wxlogR::load_CS_1000(path = '../../field-downloads/fortress/hanging tree/hi freq/clean/TOA5_FRG_low_20220214.flux15.dat',
-                            skip_4_dat = 4)
+# check data gaps against complete time series range
+date_range <- range(bind$TIMESTAMP)
+date_range
 
-out <- rbind(ec1, ec2, ec3)
+date_seq <- as.POSIXct(seq(date_range[1], date_range[2], by = 15 * 60))
 
-write.csv(out, 'data/eddy_cov_15min_combined.csv')
-saveRDS(out, 'data/eddy_cov_15min_combined.rds')
+missing_records <- length(date_seq) - nrow(bind)
+
+print(paste('The number of records missing is: ', missing_records))
+
+# join on full sequence
+date_seq_df <- data.frame(TIMESTAMP = date_seq)
+
+df_cont <- date_seq_df |> 
+  left_join(bind, by = 'TIMESTAMP')
+
+write.csv(df_cont, 'data/eddy_cov_15min_combined.csv')
+saveRDS(df_cont, 'data/eddy_cov_15min_combined.rds')
