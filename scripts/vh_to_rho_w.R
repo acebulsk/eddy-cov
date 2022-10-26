8bf470aee4fa2b3d624b5b40fe600de719d14ecb
Alex Cebulski <alexcebulski@gmail.com>
2022-10-26 17:11:06 -0600

    IMPORTANT update to include correct mV to rho w calculation from KH20 manual A-3

--- scripts/vh_to_rho_w.R
+++ scripts/vh_to_rho_w.R
@@ -9,18 +9,36 @@ l <- list.files('data/hi-freq/raw/subset-15min-ascii', full.names = F)
 
 # constants from logger program
 
-XKW_high <- -0.181 # sensor specific S/N 1527, scaled, dry vapour range [m^3 / (g cm)]
+V_0 <- 3259 # Input Voltage at KH20 (according to table A-2 in KH20-manual for dry conditions) [mV]
+KW_high <- 0.181 # sensor specific S/N 1527, scaled, dry vapour range [m^3 / (g cm)]
 x <- 1  # Path length of KH20 (cm)
-xkw <- (-1) * x * XKW_high  # Path Length * Water Vapor absorbtion coefficient (m^3 / g)
+xkw <- x * XKW_high  # Path Length * Water Vapor absorption coefficient (m^3 / g)
 
-df <- wxlogR::load_CS_1000(paste0(raw_path, l[1]), timezone = 'GMT-6')
+# look at one file to check calculations 
 
-df$rho_w <- log(df$vh) / xkw # g / m3
+# df <- wxlogR::load_CS_1000(paste0(raw_path, l[1]), timezone = 'GMT-6')
+# 
+# df$ln_mV <- log(df$vh) # check KH20 manual fig. A-1 to ensure log(mV) aka log(vh) matches the vapor density (g.m3)
+# 
+# df$rho_w <- (log(df$vh) - log(V_0)) / xkw # g / m3, equation is A-3 from KH20 manual
+
+# check calculated water vapour density against absolute humidity, not sure if this is appropriate comparison as does not match up well for Feb 10, 2022 - maybe instrument error. 
+
+# met <- readRDS('../../analysis/interception/data/met/met_main_th.rds') |> 
+#   select(TIMESTAMP = datetime, AirTC_towerTop, RH_towerTop) |> 
+#   mutate(TIMESTAMP = round(TIMESTAMP, "mins"))
+# 
+# ec <- df |> mutate(TIMESTAMP = round(TIMESTAMP, "mins"))
+# 
+# ec_met <- left_join(ec, met)
+# 
+# ec_met$rho_air <- psychRomet::absolute_humidity(ec_met$AirTC_towerTop, ec_met$RH_towerTop/100) * 1000
+
 
 calc_rho_w <- function(file) {
   df <- wxlogR::load_CS_1000(paste0(raw_path, file), timezone = 'GMT-6')
   
-  df$rho_w <- log(df$vh) / xkw
+  df$rho_w <- (log(df$vh) - log(V_0)) / xkw # g / m3, equation is A-3 from KH20 manual
   
   df <- df[c('Ux', 'Uy', 'Uz', 'Ts', 'rho_w')]
   
