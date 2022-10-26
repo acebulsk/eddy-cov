696de8f575a80aaa7bfefd701311a2374a696684
Alex Cebulski <alexcebulski@gmail.com>
2022-10-26 17:31:45 -0600

    update the xkw sign to the way it is in the CSI manual

--- scripts/vh_to_rho_w.R
+++ scripts/vh_to_rho_w.R
@@ -9,10 +9,15 @@ l <- list.files('data/hi-freq/raw/subset-15min-ascii', full.names = F)
 
 # constants from logger program
 
-V_0 <- 3259 # Input Voltage at KH20 (according to table A-2 in KH20-manual for dry conditions) [mV]
+V_0 <- 3259 # Input Voltage at KH20 (according to table A-2 in KH20-manual for dry conditions) [mV] Should update to value on calibration sheet. 
 KW_high <- 0.181 # sensor specific S/N 1527, scaled, dry vapour range [m^3 / (g cm)]
 x <- 1  # Path length of KH20 (cm)
 xkw <- x * XKW_high  # Path Length * Water Vapor absorption coefficient (m^3 / g)
+
+# compare to lindsay nightmare email chain
+
+(log(870.5355) - log(V_0)) / -0.185
+(log(490.8061) - log(V_0)) / -0.185
 
 # look at one file to check calculations 
 
@@ -38,7 +43,7 @@ xkw <- x * XKW_high  # Path Length * Water Vapor absorption coefficient (m^3 / g
 calc_rho_w <- function(file) {
   df <- wxlogR::load_CS_1000(paste0(raw_path, file), timezone = 'GMT-6')
   
-  df$rho_w <- (log(df$vh) - log(V_0)) / xkw # g / m3, equation is A-3 from KH20 manual
+  df$rho_w <- (log(df$vh) - log(V_0)) / -xkw # g / m3, equation is A-3 from KH20 manual
   
   df <- df[c('Ux', 'Uy', 'Uz', 'Ts', 'rho_w')]
   
